c2a86f954ad95e391da1eecd2ed92f3d3d6a91f3
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2022-12-30 12:46:56 +0900

    既読機能の実装

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import signupfunc, loginfnc, listfunc, logoutfunc, detailfunc
+from .views import signupfunc, loginfnc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc
 
 urlpatterns = [
     path("signup/", signupfunc, name='signup'),
     path('login/', loginfnc, name='login'),
     path('list/', listfunc, name='list'),
     path('logout/', logoutfunc, name='logout'),
-    path('detail/<int:pk>', detailfunc, name='detail')
+    path('detail/<int:pk>', detailfunc, name='detail'),
+    path('good/<int:pk>', goodfunc, name='good'),
+    path('read/<int:pk>', readfunc, name='read')
 ]
